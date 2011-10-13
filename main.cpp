50b916e1f13a8f0821034538026ad92b4d0fa9be
Benjamin Mayes <bdm8233@rit.edu>
2011-10-13 09:29:44 -0400

    Increased platform independence.

--- main.cpp
+++ main.cpp
@@ -8,8 +8,15 @@
 
 #include <iostream>
 #include <cmath>
+#ifdef __APPLE__
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#else
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#endif
 #include "complex.h"
 
 using namespace std;
